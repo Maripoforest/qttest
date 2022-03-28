d67ee6a28dfef7a7dc5699cc828eaea89b46110b
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-28 20:32:55 +0100

    Update mywidget.h

--- mywidget.h
+++ mywidget.h
@@ -12,32 +12,36 @@
 class MyWidget : public QWidget
 {
     Q_OBJECT
+
 public:
     MyWidget();
-    void hasValue(float* value);
     float pixel[32][24] = {0};
+    void hasValue(float* value);
  
 protected:
-    void paintEvent(QPaintEvent *event);
-    //void timerEvent(QTimerEvent *);
-    int count;
-   
+    void paintEvent(QPaintEvent* event);
     void timerEvent(QTimerEvent*);
    
 signals:
+
 private:
     QPushButton  *button;
     QwtThermo    *thermo;
     QVBoxLayout  *vLayout;  // vertical layout
     QHBoxLayout  *hLayout;  // horizontal layout
-    QwtPlot *plot;
+    QwtPlot      *plot;     /* not used in this graph, comment shall we ? */
 
-    int intervalTime = 200;
-
+    int intervalTime = 1000;
+    double pixel_max =0;
+    float Pixel =0;  
+    float temp;
+    int colorR = 0;
+    int colorG = 0;
+    int colorB = 0;
+    
     void reset();
 
 public slots:
  
 };
 #endif
-
