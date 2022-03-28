2996c61fcba156001797d14d7dc7fd14056ddbf9
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-28 20:23:23 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdint>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include "mlx90640_driver.h"
+#include "mywidget.h"
+
+#include <QtGui>
+#include <QApplication>
+
+
+int main(int argc, char** argv){
+
+    QApplication app( argc, argv);
+    MyWidget mywidget;
+    MLX90640 mlx90640;
+    
+    mlx90640.registerCallback(&mywidget);
+    mlx90640.start();
+    getchar();
+    mlx90640.stop();
+
+    return 0;
+}
